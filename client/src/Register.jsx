edf0280b691c3790e3601141e4ee9ce32bae123c
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-08 15:24:40 +0100

    created Login and Register components, along with form UI

---
+++ client/src/Register.jsx
@@ -0,0 +1,91 @@
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import axios from 'axios';
+
+const Register = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:5000/register', {name, email, password})
+      .then(result => console.log(result))
+      .catch(err=> console.log(err))
+  }
+
+
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2> Register</h2>
+
+      <form onSubmit= {handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name">
+            {" "}
+            <strong> Name </strong>{" "}
+          </label>
+          <input
+            className="form-control rounded-0"
+            type="text"
+            id={name}
+            name="name"
+            placeholder="Enter Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="email">
+            {" "}
+            <strong> Email </strong>{" "}
+          </label>
+          <input
+            className="form-control rounded-0"
+            type="email"
+            id={email}
+            name="email"
+            placeholder="Enter Email"
+            onChange= {(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="password">
+            {" "}
+            <strong> Password </strong>{" "}
+          </label>
+          <input
+            className="form-control rounded-0"
+            type="password"
+            id={password}
+            name="password"
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button
+          className="btn btn-success w-100 rounded-0"
+          type="submit"
+          name="register"
+          value="Register"
+        >
+          {" "}
+          Register{" "}
+        </button>
+
+    </form>
+
+        <p> Already Have an Account? </p>
+        <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none" type="button" name="login" value="Login">
+          {" "}
+          Login{" "}
+        </Link>
+    </div>
+</div>
+  );
+};
+
+export default Register;
